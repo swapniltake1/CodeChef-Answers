3bf28bc1a8940d89a4645f6ca5c445641b31630d
SWAPNIL TAKE <swapniltake1@outlook.com>
2022-09-13 14:53:49 +0530

    Problem Code: FEVER Codechef
    Chef is not feeling well today. He measured his body temperature using a thermometer and it came out to be XX °F.
    
    A person is said to have fever if his body temperature is strictly greater than 9898 °F.
    
    Determine if Chef has fever or not.
    
    Input Format
    The first line contains a single integer TT — the number of test cases. Then the test cases follow.
    The first and only line of each test case contains one integer XX - the body temperature of Chef in

---
+++ FEVER.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t, f;
+	cin >> t;
+	while(t--){
+	    cin >> f;
+	    if(f>98){
+	        cout << "YES" << endl;
+	    }else{
+	        cout << "NO" <<endl;
+	    }
+	}
+	return 0;
+}
